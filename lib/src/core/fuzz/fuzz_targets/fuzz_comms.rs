f1b07beb7d11ea49e4128c39a60dfed31ffde873
britton <brittonkfischer@gmail.com>
2022-05-12 11:34:14 -0700

    Update Fuzz Testing Code

--- lib/src/core/fuzz/fuzz_targets/fuzz_comms.rs
+++ lib/src/core/fuzz/fuzz_targets/fuzz_comms.rs
@@ -5,19 +5,22 @@ use std::sync::Arc;
 
 use bytes::BytesMut;
 use tokio_util::codec::Decoder;
-
-use crate::core::prelude::*;
+use opcua::server::prelude::Message;
+use opcua::types::DecodingOptions;
+use opcua::sync::RwLock;
+use opcua::server::prelude::TcpCodec;
+use opcua::core::prelude::*;
 
 pub fn decode(buf: &mut BytesMut, codec: &mut TcpCodec) -> Result<Option<Message>, std::io::Error> {
     codec.decode(buf)
 }
 
 fuzz_target!(|data: &[u8]| {
-    crate::console_logging::init();
+    //crate::console_logging::init();
     // With some random data, just try and deserialize it
     let decoding_options = DecodingOptions::default();
     let abort = Arc::new(RwLock::new(false));
-    let mut codec = TcpCodec::new(abort, decoding_options);
+    let mut codec = TcpCodec::new(decoding_options);
     let mut buf = BytesMut::from(data);
     let _ = decode(&mut buf, &mut codec);
 });
